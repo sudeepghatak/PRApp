96bfcd7cd7c0d6380a3c18153d6689fa5bcf1065
ghataks@moe.govt.nz <ghataks@moe.govt.nz>
2023-07-13 15:52:34 +1200

    Class file

--- PR_Application/src/webparts/prApp/components/IPrAppState.ts
+++ PR_Application/src/webparts/prApp/components/IPrAppState.ts
@@ -1,7 +1,8 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 import{SPHttpClient} from "@microsoft/sp-http";
+import { IPRItem } from "./IPRItem";
 
 export interface IPrAppState {
-  ListItems:any[];
+  ListItems:IPRItem[];
   LoggedInUser:any;
 }
