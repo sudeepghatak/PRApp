3c0c4493aa38c411a5397254987007b77eb26e8e
unknown <chatterjeeshohini@gmail.com>
2023-07-19 14:11:12 +0530

    New Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Restaurent from './Components/Basics/Restaurent';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>       {/* or <></> --- react fragment  */}
+      <Restaurent/>
+      <p className='Description'>now,you entered project {5+3}</p>
+      <MyApp/> 
     </div>
-  );
-}
+  )
+};
+const MyApp =() =>{
+  return(
+    <>
+    <p>here we are decleare our main code </p>
+    </>
+  )
+};
 
-export default App;
+export default App
---
+++ src/Components/Basics/Restaurent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Restaurent = () => {
+  return (
+    <div>
+      <h1> Hello,welcome to our restaurent </h1>
+    </div>
+  )
+}
+
+export default Restaurent
